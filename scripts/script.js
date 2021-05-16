6684fc391b2089bc83840bd4cb03ca97787c4599
Kalidasan Dheeraj <marslandersltd@gmail.com>
2021-05-16 17:18:10 +0530

    Complete Part 1

--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,45 @@ const setState = router.setState;
 
 // Make sure you register your service worker here too
 
+let entryNum = 0;
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
     .then(response => response.json())
     .then(entries => {
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
+        entryNum += 1;
+        let currentEntryNum = entryNum;
+        newPost.addEventListener('click', () => {
+          history.pushState({currentState: 2, entryNumber: currentEntryNum, entryItem: entry}, "entries", "/#entry" + currentEntryNum);
+          router.setState(2, currentEntryNum, entry);
+        });
         newPost.entry = entry;
         document.querySelector('main').appendChild(newPost);
       });
     });
 });
+
+const settingsIcon = document.getElementsByTagName('header')[0].children[1];
+
+settingsIcon.addEventListener("click", () => {
+  window.history.pushState({currentState: 3, entryNumber: 0, entryItem: null}, "settings", "/#settings"); //TODO: TEST THIS
+  router.setState(3, 0, null);
+});
+
+const headingTag = document.getElementsByTagName('h1')[0];
+headingTag.addEventListener("click", () => {
+  window.history.pushState({currentState: 1, entryNumber: 0, entryItem: null}, "home", "/"); //TODO: TEST THIS
+  router.setState(1, 0, null);
+});
+
+window.onpopstate = function(event) {
+  if(event.state.currentState === 1){
+    router.setState(1, 0, null);
+  } else if (event.state.currentState === 3) {
+    router.setState(3, 0, null);
+  } else {
+    router.setState(2, event.state.entryNumber, event.state.entryItem);
+  }
+}
+
